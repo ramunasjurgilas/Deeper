08bddb4eb67efb6b4f4a353a8dfcd1912d2847a8
Ramūnas Jurgilas <ramunas.jurgilas@gmail.com>
2025-04-03 00:46:52 +0300

    BathymetryMapView buvo pridėtas.

---
+++ Deeper/BathymetryMapView.swift
@@ -0,0 +1,18 @@
+//
+//  BathymetryMapView.swift
+//  Deeper
+//
+//  Created by Ramunas Jurgilas on 03/04/2025.
+//
+
+import SwiftUI
+
+struct BathymetryMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BathymetryMapView()
+}
