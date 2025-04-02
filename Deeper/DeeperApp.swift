0faa09f0636505f1a83e7c5908a501d08d25aaba
Ramūnas Jurgilas <ramunas.jurgilas@gmail.com>
2025-04-02 22:11:35 +0300

    Initial Commit

---
+++ Deeper/DeeperApp.swift
@@ -0,0 +1,20 @@
+//
+//  DeeperApp.swift
+//  Deeper
+//
+//  Created by Ramunas Jurgilas on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeeperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
